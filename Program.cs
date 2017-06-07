569f515a5eaceefc9780bfe01299da4e06fd02ff
Theresa Skonieczny <tsscamaro@gmail.com>
2017-06-07 16:53:06 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //This creates/istatiates the object of the Triangle Class
+            Triangle t1 = new Triangle();
+            
+            //Message displayed to user
+            Console.WriteLine("Enter the length of one side for the triangle : ");
+            //Converts the user input into a string
+            string Side1Length = Console.ReadLine();
+
+            Console.WriteLine("Enter the second length of one side of the triangle : ");
+            string Side2Length = Console.ReadLine();
+
+            Console.WriteLine("Enter the third length of the last side of the triangle : ");
+            string Side3Length = Console.ReadLine();
+
+            //Converts the String by Parsing the data and returning a double and storing in the object.
+            t1.Side1Length = Double.Parse(Side1Length);
+            t1.Side2Length = Double.Parse(Side2Length);
+            t1.Side3Length = Double.Parse(Side3Length);
+
+            //Message sent to user, along with data supplied by user and calculations recieved from methods within the class
+            Console.WriteLine("Your triangle will have sides {0}, {1}, {2}.",t1.Side1Length, t1.Side2Length, t1.Side3Length);
+            Console.WriteLine("Area: " + t1.getFormattedArea());
+            Console.WriteLine("Perimeter: " + t1.getFormattedPerimeter());
+
+          
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_46
+{
+    class Triangle
+    {
+        //These Variables identify the basic criteria for the Triangle object
+        private double side1Length;
+        private double side2Length;
+        private double side3Length;
+        private double area;
+        private double perimeter;
+
+        //The Getters & Setters are the properties type can be accessed by constructors
+        //and are available to be referenced as public.
+
+        public double Side1Length
+        {
+            get
+            {
+                return side1Length;
+            }
+            set
+            {
+                side1Length = value;
+            }
+        }
+        public double Side2Length
+        {
+            get
+            {
+                return side2Length;
+            }
+            set
+            {
+                side2Length = value;
+            }
+        }
+        public double Side3Length
+        {
+            get
+            {
+                return side3Length;
+            }
+            set
+            {
+                side3Length = value;
+            }
+        }
+        //this constructor initiates a value for each property
+        public Triangle()
+        {
+            side1Length = 0;
+            side2Length = 0;
+            side3Length = 0;
+            
+           
+        }
+       
+        //this constructor enables the user to input the value of the property
+        public Triangle(double Input1Length, double Input2Length, double Input3Length)
+        {
+            side1Length = Input1Length;
+            side2Length = Input2Length;
+            side3Length = Input3Length;
+        }
+        //the getArea Property below performs GETS the data to calculate the Area of a Triangle
+        public double getArea
+        {
+            get { return .5 * side1Length * side2Length; }
+        }
+        //the getPerimeter Property performs GETS the data inputted by user for Perimeter of Triangle
+        public double getPerimeter
+        {
+            get { return side1Length + side2Length + side3Length; }
+        }
+        //This method gets the information obtained, Returns the STRING of Area 
+        public string getFormattedArea()
+        {
+            return (getArea.ToString());
+        }
+        //This method gets the information obtained, Returns the STRING of Perimeter
+        public string getFormattedPerimeter()
+        {
+            return (getPerimeter.ToString());
+        }
+
+               
+        }
+    }
